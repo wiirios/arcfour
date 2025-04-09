0a821fc000388aef763ac6a7774567d747f45fd2
Winnd11 <recuperacaowilliamrios@gmail.com>
2025-04-09 17:12:02 -0300

    feat: declare encrypt and decrypt functions

--- src/arcfour.h
+++ src/arcfour.h
@@ -18,6 +18,8 @@ struct s_arcfour
 struct s_arcfour* create_arc(unsigned char *s, char *key, size_t key_size);
 void arcfour_ksa(unsigned char *s, char *key, size_t key_size);
 void arcfour_prga(unsigned char *s, size_t key_size, unsigned char *keystream, size_t secret_size);
+void encrypt(size_t plaintext_size, char *cipher, char *keystream, char *plaintext);
+void decrypt(size_t plaintext_size, char *cipher, char *keystream, char *deciphered);
 
 int fatal(char *msg);
 
