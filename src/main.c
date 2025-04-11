200bb67664e4a6b6aef7e046301438d8f9a3f361
Winnd11 <recuperacaowilliamrios@gmail.com>
2025-04-11 17:22:57 -0300

    refactor: simplify ARCFOUR test by using encrypt and decrypt funct

--- src/main.c
+++ src/main.c
@@ -1,8 +1,10 @@
 #include "arcfour.h"
 
+/* test only */
+
 int main(int argc, char *argv[]) {
-    unsigned char *key = argv[1];
-    unsigned char plaintext[] = "Plaintext"; 
+    unsigned char *plaintext = argv[1]; 
+    unsigned char *key = argv[2];
 
     size_t key_size = strlen(key);
     size_t plaintext_size = strlen(plaintext);
@@ -17,15 +19,14 @@ int main(int argc, char *argv[]) {
     /* generate keystream */
     arcfour_ksa(s_->s, s_->key, s_->key_size);
     arcfour_prga(s_->s, s_->key_size, &keystream[0], plaintext_size);
-    
-    for (int i = 0; i < plaintext_size; i++) {
-        cipher[i] = plaintext[i] ^ keystream[i];
-    }
 
-    for (int i = 0; i < plaintext_size; i++) {
-        deciphered[i] = cipher[i] ^ keystream[i];
-    }
-    
+    /* encrypt */
+    encrypt(plaintext_size, &cipher[0], &keystream[0], &plaintext[0]);
+
+    /* decrypt */
+    decrypt(plaintext_size, &cipher[0], &keystream[0], &deciphered[0]);
+
+
     free(s_);
 
     return 0;
